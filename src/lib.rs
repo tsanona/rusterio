29622f0fca52dacce42b3c369340997af6b0feff
Tiago Sanona <tsanona@gmail.com>
2024-12-28 22:05:04 +0100

    implement read_bands

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,51 @@
 #![allow(dead_code)]
 
-use gdal::{
-    errors::GdalError, raster::Buffer, Dataset, GeoTransform, GeoTransformEx, Metadata,
-    MetadataEntry,
-};
-use ndarray::{Array2, ShapeError};
-use proj::{Proj, ProjCreateError};
+use gdal::{errors::GdalError, Dataset, Metadata, MetadataEntry};
+use itertools::Itertools;
+use ndarray::{Array2, Array3, ShapeError};
+use nalgebra::Point2;
+use proj::ProjCreateError;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::{
     collections::HashMap,
     path::{Path, PathBuf},
 };
-use thiserror::Error;
-use tokio_stream::StreamExt;
+use thiserror;
 
-#[derive(Error, Debug)]
+use rasters;
+use rasters::prelude::{
+    transform_from_gdal, transform_window, ChunkConfig, ChunkReader, DatasetReader, PixelTransform
+};
+
+pub type Result<T> = std::result::Result<T, RasterError>;
+
+#[derive(thiserror::Error, Debug)]
 pub enum RasterError {
     #[error(transparent)]
     GdalError(#[from] GdalError),
     #[error(transparent)]
-    ShapeError(#[from] ShapeError),
-    #[error(transparent)]
     ProjError(#[from] ProjCreateError),
+    #[error(transparent)]
+    RastersError(#[from] rasters::Error),
+    #[error(transparent)]
+    ShapeError(#[from] ShapeError),
     #[error("Band `{0}` not found.")]
     BandNotFound(String),
+    #[error("Band `{0}` has a non inverteble geo transform.")]
+    BandTransformNotInvertible(String),
     #[error("Couldn't find metadata key {key} in dataset {dataset_path}.")]
     MetadataKeyNotFound { dataset_path: String, key: String },
 }
+
+type RasterMetadata = HashMap<String, String>;
+type BandName = String;
+type BandsInfo = HashMap<BandName, BandInfo>;
 
 #[derive(Debug)]
 pub struct Raster {
     path: PathBuf,
     metadata: RasterMetadata,
-    bands_info: HashMap<String, BandInfo>,
+    bands_info: BandsInfo,
 }
 
 impl Raster {
@@ -40,41 +54,29 @@ impl Raster {
     }
 }
 
+type BandMetadata = HashMap<String, String>;
+
 #[derive(Debug)]
 struct BandInfo {
     index: usize,
-    metadata: BandMetadata,
-    bounds: Bounds,
-    projection: Proj,
-    geo_transform: GeoTransform,
     path: PathBuf,
+    chunk_config: ChunkConfig,
+    metadata: BandMetadata,
+    geo_transform: PixelTransform,
 }
 
 impl BandInfo {
-    fn dataset(&self) -> gdal::errors::Result<Dataset> {
-        Dataset::open(&self.path)
-    }
-    fn resolution(&self) -> usize {
-        self.geo_transform[1] as usize
+    pub fn dataset(&self) -> Result<Dataset> {
+        Dataset::open(&self.path).map_err(RasterError::GdalError)
     }
 }
 
-#[derive(Debug)]
-struct Bounds {
-    xmin: f64,
-    xmax: f64,
-    ymin: f64,
-    ymax: f64,
-}
-
-type RasterMetadata = HashMap<String, String>;
-type BandMetadata = HashMap<String, String>;
 type RasterSubDatasets = Vec<Dataset>;
 
 impl Raster {
     const BANDNAME_KEY: &'static str = "BANDNAME";
 
-    pub fn new<P: AsRef<Path>>(path: P) -> Result<Raster, RasterError> {
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Raster> {
         let dataset = Dataset::open(&path)?;
         let (metadata, subdatasets) = Self::parse_dataset(&dataset)?;
         let bands_info = subdatasets
@@ -93,9 +95,60 @@ impl Raster {
         })
     }
 
+    fn bands_info(&self, band: &str) -> Result<&BandInfo> {
+        self.bands_info
+            .get(band)
+            .ok_or(RasterError::BandNotFound(band.into()))
+    }
+
+    pub fn read_bands(
+        &self,
+        bands: Vec<&str>,
+        offset: (isize, isize),
+        window: (usize, usize),
+    ) -> Result<Array3<u16>> {
+        let highest_resolution_transform = bands
+            .iter()
+            .map(|band| {
+                self.bands_info(band)
+                    .map(|band_info| band_info.geo_transform)
+            })
+            .process_results(|bands_info| {
+                bands_info.reduce(|prev, next| if prev.m11 < next.m11 { prev } else { next })
+            })?
+            .unwrap();
+
+        let band_rasters = bands
+            .par_iter()
+            .map(|band| {
+                let band_info = self.bands_info(band)?;
+                let transform = band_info
+                    .geo_transform
+                    .try_inverse()
+                    .ok_or(RasterError::BandTransformNotInvertible((*band).into()))?
+                    * highest_resolution_transform;
+                let (corrected_offset, corrected_window) = transform_window(
+                    (offset, window),
+                    transform,
+                    band_info.dataset()?.raster_size(),
+                );
+                DatasetReader(band_info.dataset()?, band_info.index)
+                    .read_as_array::<u16>(corrected_offset, corrected_window)
+                    .map(|band_raster| (band_raster, transform))
+                    .map_err(RasterError::RastersError)
+            })
+            .collect::<Result<Vec<(Array2<u16>, PixelTransform)>>>()?;
+        Ok(Array3::from_shape_fn((bands.len(), window.0, window.1), 
+        |(c, x, y)| {
+            let (band_raster, transform) = &band_rasters[c];
+            let corrected_coords = transform.transform_point(&Point2::new(x as f64, y as f64));
+            band_raster[[corrected_coords.x as usize, corrected_coords.y as usize]]
+        }))
+    }
+
     fn parse_dataset(
         dataset: &Dataset,
-    ) -> Result<(RasterMetadata, RasterSubDatasets), RasterError> {
+    ) -> Result<(RasterMetadata, RasterSubDatasets)> {
         let mut raster_metadata = RasterMetadata::new();
         let mut raster_subdataset_paths = RasterSubDatasets::new();
         for MetadataEntry { domain, key, value } in dataset.metadata() {
@@ -112,11 +165,11 @@ impl Raster {
         Ok((raster_metadata, raster_subdataset_paths))
     }
 
-    fn parse_subdataset(dataset: Dataset) -> Result<HashMap<String, BandInfo>, RasterError> {
+    fn parse_subdataset(dataset: Dataset) -> Result<HashMap<String, BandInfo>> {
         let mut bands_info = HashMap::new();
-        for (idx, raster_band) in dataset.rasterbands().filter_map(Result::ok).enumerate() {
+        for (idx, raster_band) in dataset.rasterbands().enumerate() {
             let mut metadata = BandMetadata::new();
-            for MetadataEntry { domain, key, value } in raster_band.metadata() {
+            for MetadataEntry { domain, key, value } in raster_band?.metadata() {
                 match domain.as_str() {
                     "" => {
                         metadata.insert(key, value);
@@ -124,11 +177,11 @@ impl Raster {
                     _ => continue,
                 }
             }
-            let projection = Proj::new(dataset.spatial_ref()?.to_proj4()?.as_str())?;
-            let geo_transform = dataset.geo_transform()?;
-            let bounds =
-                Self::get_extent(raster_band.x_size(), raster_band.y_size(), geo_transform);
+            //let projection = Proj::new(dataset.spatial_ref()?.to_proj4()?.as_str())?;
+            let geo_transform = transform_from_gdal(&dataset.geo_transform()?);
             let dataset_path = dataset.description()?;
+            let chunk_config = ChunkConfig::for_dataset(&dataset, Some(idx + 1..idx + 2))
+                .map_err(RasterError::RastersError)?;
             bands_info.insert(
                 metadata
                     .get(Self::BANDNAME_KEY)
@@ -139,74 +192,15 @@ impl Raster {
                     .to_string(),
                 BandInfo {
                     index: idx + 1,
-                    metadata,
-                    bounds,
-                    projection,
-                    geo_transform,
                     path: dataset.description()?.into(),
+                    chunk_config,
+                    metadata,
+                    geo_transform,
                 },
             );
         }
         dataset.close()?;
         Ok(bands_info)
-    }
-
-    fn get_extent(width: usize, height: usize, geo_transform: GeoTransform) -> Bounds {
-        let (xmin, ymin) = geo_transform.apply(0.0, 0.0);
-        let (xmax, ymax) = geo_transform.apply(width as f64, height as f64);
-        Bounds {
-            xmin,
-            xmax,
-            ymin,
-            ymax,
-        }
-    }
-
-    // TODO: try using threads for reading bands/blocks (tokio stream?, vanilla spawn?)
-    pub async fn get_array_async(
-        &self,
-        band_names: Vec<&str>,
-        offset: (isize, isize),
-        size: (usize, usize),
-    ) -> Vec<Result<Buffer<u16>, RasterError>> {
-        tokio_stream::iter(band_names)
-            // TODO: hadle with unsuccessfull band dataset open calls
-            .map(|band_name| {
-                let band_info = self
-                    .bands_info
-                    .get(band_name)
-                    .ok_or(RasterError::BandNotFound(String::from(band_name)))?;
-                band_info
-                    .dataset()?
-                    .rasterband(band_info.index)?
-                    .read_as(offset, size, size, None)
-                    .map_err(RasterError::GdalError)
-            })
-            .collect()
-            .await
-    }
-
-    pub fn get_array(
-        &self,
-        band_names: Vec<&str>,
-        offset: (isize, isize),
-        size: (usize, usize),
-    ) -> Vec<Result<Buffer<u16>, RasterError>> {
-        band_names
-            .into_iter()
-            // TODO: hadle with unsuccessfull band dataset open calls
-            .map(|band_name| {
-                let band_info = self
-                    .bands_info
-                    .get(band_name)
-                    .ok_or(RasterError::BandNotFound(String::from(band_name)))?;
-                band_info
-                    .dataset()?
-                    .rasterband(band_info.index)?
-                    .read_as::<u16>(offset, size, size, None)
-                    .map_err(RasterError::GdalError)
-            })
-            .collect()
     }
 }
 
@@ -216,14 +210,14 @@ mod tests {
     use rstest::{fixture, rstest};
 
     const TEST_DATA: &str =
-        "data/S2B_MSIL2A_20241206T093309_N0511_R136_T33PTM_20241206T115919.SAFE.zip";
+        "data/S2B_MSIL2A_20241126T093239_N0511_R136_T33PTM_20241126T120342.SAFE.zip";
 
     #[fixture]
     fn test_raster() -> Raster {
         Raster::new(TEST_DATA).unwrap()
     }
 
-    #[rstest]
+    /*#[rstest]
     fn it_works(test_raster: Raster) {
         let b1 = test_raster.bands_info.get("B2").unwrap();
         //let b2 = raster.bands_info.get("B2").unwrap();
@@ -234,12 +228,15 @@ mod tests {
         //polygon = polygon.affine_transform(&geo::AffineTransform::from(raster.dataset().unwrap().geo_transform().unwrap()));
         //println!("{:#?}", raster.metadata.get("FOOTPRINT"));
         println!("{:#?}", b1.bounds);
-    }
+    }*/
 
     #[rstest]
-    #[tokio::test]
-    async fn play_ground(test_raster: Raster) {
-        let res = test_raster.get_array_async(vec!["B4", "B3", "B2"], (0, 0), (256, 256));
-        println!("{:#?}", res.await)
+    fn play_ground(test_raster: Raster) {
+        //windows need reajusting for bands with different resolution
+        print!(
+            "{:#?}",
+            test_raster
+                .read_bands(vec!["B4", "B3", "B2"], (0, 0), (125, 125)).unwrap()
+        );
     }
 }
