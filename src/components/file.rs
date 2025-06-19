4e201d004ccf0d0d990d0d867a38f90553a9b804
tiago <tsanona@gmail.com>
2025-06-19 18:40:38 +0200

    fix

--- src/components/file.rs
+++ src/components/file.rs
@@ -7,16 +7,16 @@ use crate::{
     Indexes,
 };
 
-pub trait File: Debug + Sized {
-    type T: DataType;
+pub trait File<T: DataType>: Debug + Sized {
+    //type T: DataType;
     fn open<P: AsRef<Path>>(path: P) -> Result<Self>;
     fn description(&self) -> Result<String>;
     fn size(&self) -> (usize, usize);
     fn crs(&self) -> String;
     fn transform(&self) -> Result<AffineTransform>;
     fn num_bands(&self) -> usize;
-    fn band(&self, index: usize) -> Result<RasterBand<Self::T>>;
-    fn bands(&self, indexes: Indexes, drop: bool) -> Result<Vec<RasterBand<Self::T>>> {
+    fn band(&self, index: usize) -> Result<RasterBand<T>>;
+    fn bands(&self, indexes: Indexes, drop: bool) -> Result<Vec<RasterBand<T>>> {
         indexes
             .into_iter(self.num_bands(), drop)
             .map(|idx| self.band(idx))
